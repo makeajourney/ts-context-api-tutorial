f5ff1b003c72a91e971dca53d6619ba9a4bc3eb0
Soyoun Kim <makeajourney@gmail.com>
2020-01-03 15:22:13 +0900

    context 이용

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import './TodoItem.css';
+import { useTodosDispatch, Todo } from '../contexts/TodosContext';
 
 export type TodoItemProps = {
-    todo: {
-        id: number;
-        text: string;
-        done: boolean;
-    };
+    todo: Todo;
 }
 
 function TodoItem({ todo }: TodoItemProps) {
+    const dispatch = useTodosDispatch();
+
+    const onToggle = () => {
+        dispatch({
+            type: 'TOGGLE',
+            id: todo.id
+        });
+    };
+
+    const onRemove = () => {
+        dispatch({
+            type: 'REMOVE',
+            id: todo.id
+        });
+    };
+
     return (
         <li className={`TodoItem ${todo.done ? 'done' : ''}`}>
-            <span className="text">{todo.text}</span>
-            <span className="remove">(X)</span>
+            <span className="text" onClick={onToggle}>{todo.text}</span>
+            <span className="remove" onClick={onRemove}>(X)</span>
         </li>
     );
 }
